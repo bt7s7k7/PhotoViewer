a3bc26ccd1301910bfc8e4e886fd5b2492647ab3
bt7s7k7 <bt7s7k7@hotmail.sk>
2019-09-07 10:24:05 +0200

    Added comments

--- PhotoViewer/src/main.cpp
+++ PhotoViewer/src/main.cpp
@@ -57,7 +57,7 @@ int main(int argc, char** argv) try {
 	double offX = 0; 
 	/* Vertical image offset */
 	double offY = 0;
-
+	/* Resets the image offset and zoom to fit inside the window, called on image reload */
 	auto resetImageTransform = [&]() {
 		auto surface = targetFile.getSurface();
 		int w, h;
@@ -73,7 +73,7 @@ int main(int argc, char** argv) try {
 			zoom = (double)h / (double)surface->h;
 		}
 	};
-
+	
 	resetImageTransform();
 
 	while (true) {
@@ -87,14 +87,15 @@ int main(int argc, char** argv) try {
 						resetImageTransform();
 					}
 				}
-
+				// Zooming using the mousewheel
 				if (event.type == SDL_MOUSEWHEEL) {
 					auto scroll = (double)event.wheel.y;
 
 					zoom += scroll * zoom * 0.1;
 				}
-
+				// Moving the image by dragging
 				if (event.type == SDL_MOUSEMOTION) {
+					// Test if the left mouse button is down
 					if (SDL_GetMouseState(nullptr, nullptr) && SDL_BUTTON(1)) {
 						offX += event.motion.xrel;
 						offY += event.motion.yrel;
@@ -102,31 +103,45 @@ int main(int argc, char** argv) try {
 				}
 
 				if (event.type == SDL_KEYDOWN) {
+					// Change to the previous image 
 					if (event.key.keysym.sym == SDLK_LEFT) {
+						/* Last valid image path */
 						std::filesystem::directory_entry last;
+						// We iterate thru the entire folder util we find the target picture, then we load the last one
 						for (const auto& file : std::filesystem::directory_iterator(targetFile.getPath().parent_path())) {
+							// Test if this is a valid file
 							if (file.is_regular_file() && allowedExtensions.find(file.path().extension().string()) != allowedExtensions.end()) {
+								// If file is our target file we load the previous one
 								if (file.path() == targetFile.getPath()) {
+									// If no previous image, give up
 									if (last.path().empty()) {
 										break;
 									} else {
+										// Loading the last image
 										targetFile = last.path();
 										resetImageTransform();
 										break;
 									}
 								} else {
+									// Save this file to be the previous file in the next iteration
 									last = file;
 								}
 							}
 						}
+					// Changind to the next image
 					} else if (event.key.keysym.sym == SDLK_RIGHT) {
+						/* If the last image was our target image */
 						bool was = false;
+						// We iterate thru the target image directory until we find our target image, the we load the one after it
 						for (const auto& file : std::filesystem::directory_iterator(targetFile.getPath().parent_path())) {
+							// Test if the file is a valid image file
 							if (file.is_regular_file() && allowedExtensions.find(file.path().extension().string()) != allowedExtensions.end()) {
+								// If the file is our target file set to load the next one
 								if (file.path() == targetFile.getPath()) {
 									was = true;
 								} else {
 									if (was) {
+										// Load this image because the previous one was the target image
 										targetFile = file.path();
 										resetImageTransform();
 										break;
@@ -140,38 +155,56 @@ int main(int argc, char** argv) try {
 		}
 
 		/// Rendering start -----------------------------------------------------------------------+
-		int width, height;
-		int iWidth, iHeight;
+		/* Window width */
+		int width; 
+		/* Window heigth */
+		int height;
+		/* Image width */
+		int iWidth; 
+		/* Image heigth */
+		int iHeight;
+		// Get window size
 		sdlhelp::handleSDLError(SDL_GetRendererOutputSize(renderer.get(), &width, &height));
+		// Set background color to black
 		sdlhelp::handleSDLError(SDL_SetRenderDrawColor(renderer.get(), 0, 0, 0, 255));
+		// Clear the window
 		sdlhelp::handleSDLError(SDL_RenderClear(renderer.get()));
-
-		int centerX = width / 2, centerY = height / 2;
+		/* Horizontal window center */
+		int centerX = width / 2; 
+		/* Vertival window center */
+		int centerY = height / 2;
 
 		{ /// Draw image --------------------------------------------------------------------------+
+			/* Target image texture */
 			auto texture = targetFile.getTexture(renderer.get());
+			// Get the image size
 			auto textureData = sdlhelp::queryTexture(texture);
 			iWidth = textureData.w;
 			iHeight = textureData.h;
-
+			/* Minimal zoom limit, calculated to fit the image inside the window */
 			double zoomLimit = std::min(std::min(
 				1.0 / ((double)iWidth / width),
 				1.0 / ((double)iHeight / height)
 			), 1.0);
-
+			// Enforce zoom limits
 			if (zoom < zoomLimit) zoom = zoomLimit;
 			if (zoom > 20) zoom = 20;
 
 			double
+				/* Width of the image after zooming */
 				w = iWidth * zoom,
+				/* Heigth of the image after zooming */
 				h = iHeight * zoom;
-
+			// Test if the image is bigger than the window horizontaly
 			if (w <= width) {
+				// If not reset the vertical offset
 				offX = 0;
 			} else {
+				// Else limit the offset to prevent black bars
 				offX = std::min(w / 2 - (double)width / 2, std::max(-w / 2 + (double)width / 2, offX));
 			}
 
+			// See previous
 			if (h <= height) {
 				offY = 0;
 			} else {
@@ -179,15 +212,19 @@ int main(int argc, char** argv) try {
 			}
 
 			double
+				/* Horizontal position of the image after offseting */
 				x = centerX - w / 2 + offX,
+				/* Vertical position of the image after offseting */
 				y = centerY - h / 2 + offY;
-
+			/* The destination rect to draw the image into */
 			SDL_Rect drawRect{
 				static_cast<int>(std::floor(x)),static_cast<int>(std::floor(y))
 				,static_cast<int>(std::floor(w))
 				,static_cast<int>(std::floor(h))
 			};
+			// Enable linear filtering if the image is smaller than its real size
 			SDL_SetHintWithPriority(SDL_HINT_RENDER_SCALE_QUALITY, zoom > 1 ? "2" : "0", SDL_HINT_OVERRIDE);
+			// Render the image
 			sdlhelp::handleSDLError(SDL_RenderCopy(renderer.get(), texture, nullptr, &drawRect));
 		}
 
@@ -195,22 +232,23 @@ int main(int argc, char** argv) try {
 		SDL_RenderPresent(renderer.get());
 		// Setting the title ----------------------------------------------------------------------+
 		SDL_SetWindowTitle(window.get(), (
-			std::string("PhotoViewer ")
-			+ std::to_string(width)
+			std::string("PhotoViewer ") // Name of the program
+			+ std::to_string(width) // Window dimesions
 			+ "x"
 			+ std::to_string(height)
 			+ " "
-			+ std::to_string(fpsMeter.fps())
+			+ std::to_string(fpsMeter.fps()) // Display the fps
 			+ "FPS :: "
-			+ targetFile.getPath().filename().string().c_str()
+			+ targetFile.getPath().filename().string().c_str() // Target file name
 			+ " "
-			+ std::to_string(iWidth)
+			+ std::to_string(iWidth) // Target file size
 			+ " x "
 			+ std::to_string(iHeight)
 			).c_str());
 
 		{ // FPS limiting -------------------------------------------------------------------------+
 			fpsMeter.tick();
+			// ms to wait to reach the target fps, this code is probably wrong because we get 100 fps instead of 60
 			double wait = (0.017 - fpsMeter.delta()) * 1000;
 			if (wait > 0)SDL_Delay(static_cast<unsigned int>(std::floor(wait)));
 		}
@@ -218,6 +256,7 @@ int main(int argc, char** argv) try {
 
 eventLoopExit: return 0;
 } catch (const std::exception & ex) {
+	// Display the error if any
 	SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "PhotoViewer critical error", ex.what(), nullptr);
 
 	return 1;
